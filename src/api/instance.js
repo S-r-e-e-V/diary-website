3fc17997648b62446a23fc122ce1db41543ef26c
Sreekanth V <47606003+S-r-e-e-V@users.noreply.github.com>
2023-01-16 00:27:07 -0500

    Update instance.js

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://diary-backend-nodejs.herokuapp.com";
+// const BASE_URL = "https://diary-backend-nodejs.herokuapp.com";
+const BASE_URL = "https://diary-vi2m.onrender.com";
 
 export default axios.create({
   baseURL: BASE_URL,
